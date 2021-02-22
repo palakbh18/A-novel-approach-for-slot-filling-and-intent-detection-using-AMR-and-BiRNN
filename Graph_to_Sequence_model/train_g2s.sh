5905a0e0d7afec6b076df14dbdfdee861918e559
palakbh18 <60910857+palakbh18@users.noreply.github.com>
2021-02-22 14:49:07 -0500

    Add files via upload

---
+++ Graph_to_Sequence_model/train_g2s.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -J g_2m -C K80 --partition=gpu --gres=gpu:1 --time=5-00:00:00 --output=train.out --error=train.err
+#SBATCH --mem=80GB
+#SBATCH -c 5
+
+export PYTHONPATH=$PYTHONPATH:/home/lsong10/ws/exp.graph_to_seq/neural-graph-to-seq-mp
+
+python src_g2s/G2S_trainer.py --config_path logs_g2s/G2S.silver_2m.config.json
+
